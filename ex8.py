679304998e76ddc85bec468a415235d9b64f5f26
ireineck <ireineck@student.cvtc.edu>
2016-01-02 16:28:00 -0600

    more stirng formatting practice

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# more complex string practice
+
+
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one","two","three","four")
+print formatter % (True,False,False,True)
+print formatter % (
+	"I had this thing,",
+	"That you could type up right.", 
+	"But it didn't sing.", 
+	"So I said goodnight."
+	)
